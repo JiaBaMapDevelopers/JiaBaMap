fd11f8df7b63ba95e229b06d77d7f1d057779314
julie <juliewah8785@gmail.com>
2024-12-24 13:58:34 +0800

    fix: 修正搜尋頁店家圖片跑版、熱門餐廳新增 loader

--- src/components/userProfile/RatingCard.vue
+++ src/components/userProfile/RatingCard.vue
@@ -2,6 +2,7 @@
 import { computed, ref, TrackOpTypes } from "vue";
 import { useRouter } from "vue-router";
 import { useKeywordStore } from "@/stores/keywordStore";
+import Loader from "../Loader.vue";
 import axios from "axios";
 
 let restaurants = ref(null)
@@ -100,69 +101,73 @@ const getRankImage = computed(() => {
 
 <template>
   <div class="box-border w-full h-screen pt-2 mt-2 overflow-y-auto sm:w-72">
-    <div>
-      <h1 class="text-2xl font-bold text-amber-500">熱門餐廳排行榜</h1>
+    <h1 class="text-2xl font-bold text-amber-500">熱門餐廳排行榜</h1>
+    <div v-if="restaurants && imageResponse">
       <p>目前所在地：{{ userLocation }}</p>
+      <div
+        v-for="(item, index) in restaurants" 
+        :key="index"
+        class="relative flex items-center gap-3 pb-2 mt-10"
+      >
+        <!-- 排名圖 -->
+        <div class="absolute w-10 h-10 -top-4" v-if="index < 3">
+          <img :src="getRankImage(index+1)" alt="店家排名">
+        </div>
+        <!-- 餐廳圖 -->
+        <div class="relative w-32 h-32 overflow-hidden">
+          <img
+            v-if="imageResponse[index]"
+            :src="imageResponse[index]"
+            alt="Restaurant Image"
+            class="absolute inset-0 object-cover w-full h-full rounded-md"
+          >
+        </div>
+        <!-- 餐廳排名、名稱 -->
+        <div class="flex flex-col justify-between sm:text-xl">
+          <div class="ml-3">
+            <h2 class="text-base font-bold text-gray-500 sm:text-xs">
+              {{ index+1 }}. 
+              <a href="#" class="text-amber-500 hover:text-orange-300">{{ item.name }}</a>
+            </h2>
+          </div>
+          <!-- 餐廳內容 -->
+          <div class="flex flex-row items-center mt-3 ml-3 space-x-1 text-center">
+
+            <div class="flex items-center w-12 h-6 px-2 space-x-1 text-center text-white bg-red-600 rounded-2xl">
+              <p class="text-xs">{{ item.rating }}</p>
+              <font-awesome-icon :icon="['fas', 'star']" class="text-xs" />
+            </div>
+
+            <p class="font-light sm:text-xs">（評論數：{{ item.userRatingCount }} 則）</p>
+            
+          </div>
+          <!-- 需判斷是否營業 -->
+          <div class="flex items-center mx-3 mt-3 text-sm">
+            <span class="mr-2 text-center text-green-600">
+              <font-awesome-icon :icon="['fas', 'circle']" style="font-size: 8px;" />
+            </span>
+            <p>{{ item?.openNow ? '營業中' : '已打烊' }}</p>
+          </div>
+
+          <div class="flex items-center mt-3 ml-3">
+            <!-- <div class="px-2 mx-1 text-base bg-gray-200 rounded-full sm:text-xs ">
+              <a href="#"
+                class="text-xs"
+                @click="navigateToSearch(item.keyword)"
+              >
+              {{ item.keyword }}
+              </a>
+            </div> -->
+            <div>
+              <p class="font-bold sm:text-xs">均消: {{ item.startPrice }} - {{ item.endPrice }}</p>
+            </div>
+          </div>    
+        </div>
+      </div>
     </div>
-    <div
-      v-for="(item, index) in restaurants" 
-      :key="index"
-      class="relative flex items-center gap-3 pb-2 mt-10"
-    >
-       <!-- 排名圖 -->
-       <div class="absolute w-10 h-10 -top-4" v-if="index < 3">
-        <img :src="getRankImage(index+1)" alt="店家排名">
-      </div>
-      <!-- 餐廳圖 -->
-      <div class="relative w-32 h-32 overflow-hidden">
-        <img
-          v-if="imageResponse[index]"
-          :src="imageResponse[index]"
-          alt="Restaurant Image"
-          class="absolute inset-0 object-cover w-full h-full rounded-md"
-        >
-      </div>
-      <!-- 餐廳排名、名稱 -->
-      <div class="flex flex-col justify-between sm:text-xl">
-        <div class="ml-3">
-          <h2 class="text-base font-bold text-gray-500 sm:text-xs">
-            {{ index+1 }}. 
-            <a href="#" class="text-amber-500 hover:text-orange-300">{{ item.name }}</a>
-          </h2>
-        </div>
-        <!-- 餐廳內容 -->
-        <div class="flex flex-row items-center mt-3 ml-3 space-x-1 text-center">
-
-          <div class="flex items-center w-12 h-6 px-2 space-x-1 text-center text-white bg-red-600 rounded-2xl">
-            <p class="text-xs">{{ item.rating }}</p>
-            <font-awesome-icon :icon="['fas', 'star']" class="text-xs" />
-          </div>
-
-          <p class="font-light sm:text-xs">（評論數：{{ item.userRatingCount }} 則）</p>
-          
-        </div>
-        <!-- 需判斷是否營業 -->
-        <div class="flex items-center mx-3 mt-3 text-sm">
-          <span class="mr-2 text-center text-green-600">
-            <font-awesome-icon :icon="['fas', 'circle']" style="font-size: 8px;" />
-          </span>
-          <p>{{ item?.openNow ? '營業中' : '已打烊' }}</p>
-        </div>
-
-        <div class="flex items-center mt-3 ml-3">
-          <!-- <div class="px-2 mx-1 text-base bg-gray-200 rounded-full sm:text-xs ">
-            <a href="#"
-              class="text-xs"
-              @click="navigateToSearch(item.keyword)"
-            >
-            {{ item.keyword }}
-            </a>
-          </div> -->
-          <div>
-            <p class="font-bold sm:text-xs">均消: {{ item.startPrice }} - {{ item.endPrice }}</p>
-          </div>
-        </div>    
-      </div>
+    <div v-else>
+      <Loader />
+      <p class="font-bold text-center text-amber-500">Loading...</p>
     </div>
   </div>
 </template>
