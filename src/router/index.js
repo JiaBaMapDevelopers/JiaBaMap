f1eabc0b0a3ad486af05eedccff93cee0d9c331f
Yoyo <apple5964315@gmail.com.tw>
2025-01-02 16:34:36 +0800

    fix: 優化header當搜尋欄打開時關閉選單、footer樣式優化、統一script在上方、修復撰寫食記header bug、header手機下拉樣式調整、更動複製頭貼存放位置 feat: 新增後臺編輯菜單畫面

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,7 @@ import CreateNoteView from "@/views/CreateNoteView.vue";
 import PreviewNoteView from "@/views/PreviewNoteView.vue";
 import Dashboard from "@/views/DashboardView.vue";
 import StoreCart from "@/views/StoreCartView.vue";
-import StoreSignUp from "../views/StoreSignUp.vue";
+// import StoreSignUp from "../views/StoreSignUp.vue";
 
 const routes = [
   {
@@ -78,12 +78,18 @@ const routes = [
     name: "storecart",
     component: StoreCart,
   },
+  // {
+  //   path: "/storesignup",
+  //   name: "storesignup",
+  //   component: StoreSignUp,
+  // },
   {
-    path: "/storesignup",
-    name: "storesignup",
-    component: StoreSignUp,
-  },
-];
+    path: '/menu-management',
+    name: 'MenuManagement',
+    component: () => import('../views/MenuManagement.vue')
+  }
+]
+
 
 const router = createRouter({
   history: createWebHistory(),
--- src/views/CreateNoteView.vue
+++ src/views/CreateNoteView.vue
@@ -1,10 +1,11 @@
+<script setup>
+import CreateNote from '@/components/CreateNote/CreateNote.vue';
+</script>
+
 <template>
-    <div class="bg-gray-100 min-h-screen flex items-center justify-center">
-      <CreateNote />
-    </div>
-  </template>
-  
-  <script setup>
-  import CreateNote from '@/components/CreateNote/CreateNote.vue';
-  </script>
+  <div class="bg-gray-100 min-h-screen flex items-center justify-center">
+    <CreateNote />
+  </div>
+</template>
+
   
