463547a84727fd164ebe8c4eb8907071679a05eb
han <qoo98471@gmail.com>
2025-01-04 21:54:43 +0800

    feat:店家評論前後端串接

--- src/components/storeComment/CommentInput.vue
+++ src/components/storeComment/CommentInput.vue
@@ -1,120 +1,159 @@
-<template>
-    <Login :visible="showLoginModal" @close="closeLoginModal" />
-    <div class="flex gap-5 mb-4">
-        <div class="flex-shrink-0 w-16 h-16 rounded-full bg-slate-300">
-            <img src="../../assets/default_user.png" alt="avatar" class="w-full h-auto overflow-hidden ">
-        </div>
-        <div class="flex flex-1 md:flex-none md:basis-2/3">
-            <div class="w-full text-left md:w-96">
-                <button @click="openComment" class="p-2 border-2 border-solid rounded-lg border-amber-500 text-amber-500">留下您對餐廳的評論</button>
-                <Stars class="my-2" />
-                <div class="flex flex-col relative" v-if="isExpanded">
-                    <div v-if="user.userData">
-                    <textarea
-                        v-model="commentText"
-                        maxlength="200"
-                        class="w-full h-20 border-2 border-solid rounded-sm resize-none"
-                        placeholder="發表用餐經驗"
-                    ></textarea>
-                    <p v-if="commentText.length >= 1" class="text-sm text-slate-500">
-                        還可以輸入 {{ 200 - commentText.length }} 字
-                    </p>
-                    <div class="flex items-center w-full h-10 my-2">
-                        <input type="number" v-model="price" placeholder="輸入用餐價格" class="flex-1 h-10 border-2 border-solid rounded-sm">
-                        <label class="ml-2">元 / 人</label>
-                    </div>
-                    <UploadPic />
-                    <button @click="submitComment" class="w-full p-2 my-2 font-bold rounded-lg shadow md:flex-1 text-amber-500">送出評論</button>
-                    </div>
-                    <div v-else class="flex justify-center items-center bg-white h-[100px] w-[250px] border-gray-400 border-2 rounded-xl">
-                        <p class="font-black">請先<button class="m-2 text-orange-400" @click="openLoginModal">登入</button>以留下您的評論!</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</template>
-
 <script setup>
-import axios from "axios"
-import { ref } from 'vue'
-import Stars from "./Stars.vue"
-import UploadPic from "./UploadPic.vue"
-import { useStarsStore } from '../../stores/starStore';
-import { useCommentStore } from '../../stores/commentStore';
-import { usePicStore } from '../../stores/picStore';
-import { useAuth } from '@/stores/authStore'
-import { useStore } from "@/stores/storePage"
-import Login from '@/components/Login.vue';
+import axios from "axios";
+import { ref } from "vue";
+import Stars from "./Stars.vue";
+import UploadPic from "./UploadPic.vue";
+import { useStarsStore } from "../../stores/starStore";
+import { useCommentStore } from "../../stores/commentStore";
+import { usePicStore } from "../../stores/picStore";
+import { useAuth } from "@/stores/authStore";
+import { useStore } from "@/stores/storePage";
+import Login from "@/components/Login.vue";
 
-const user = useAuth()
-const store = useStore()
-const { userData, userId } = user
+const user = useAuth();
+const store = useStore();
+const { userData, userId } = user;
 
-const time = new Date()
-const price = ref('')
-const commentText = ref('')
-let isExpanded = ref(false)
+const time = new Date();
+const price = ref("");
+const commentText = ref("");
+let isExpanded = ref(false);
 
 // 引入 Pinia Store
-const starsStore = useStarsStore()
-const commentStore = useCommentStore()
-const picStore = usePicStore()
+const starsStore = useStarsStore();
+const commentStore = useCommentStore();
+const picStore = usePicStore();
 
-const submitComment = async() => {
-    if(!commentText.value.trim()){
-        alert('請輸入評論')
-        return
-    }
+const submitComment = async () => {
+  if (!commentText.value.trim()) {
+    alert("請輸入評論");
+    return;
+  }
 
-    const newComment = {
-        userId: userId,
-        placeId: store.placesId,
-        content: commentText.value,
-        rating: starsStore.selectIndex,
-        AvgPrice: price.value,
-    }
-    console.log(newComment);
-    
-    await axios.post(`${import.meta.env.VITE_BACKEND_BASE_URL}/comments/`, newComment)
-    
-    commentStore.addComment(newComment); // 使用 Pinia Store 更新評論
-    //重置輸入框
-    commentText.value = ''
-    price.value = ''
-    picStore.resetPic() // 重置圖片
-    starsStore.resetStars() // 重置星星狀態
-}
+  const formData = new FormData();
+
+  formData.append("userId", userData._id); // 用戶 ID
+  formData.append("placeId", store.placesId); // 餐廳 ID
+  formData.append("content", commentText.value.trim()); // 評論內容
+  formData.append("rating", starsStore.selectIndex); // 評分
+  formData.append("AvgPrice", price.value); // 用餐價格
+
+  if (picStore.files.length > 0) {
+    picStore.files.forEach((file) => {
+      formData.append("photos", file); // "files" 是後端字段名
+    });
+  }
+
+  console.log(formData);
+
+  await axios.post(
+    `${import.meta.env.VITE_BACKEND_BASE_URL}/comments/`,
+    formData,
+    {
+      headers: { "Content-Type": "multipart/form-data" },
+    },
+  );
+
+  // commentStore.addComment(newComment); // 使用 Pinia Store 更新評論
+  //重置輸入框
+  commentText.value = "";
+  price.value = "";
+  picStore.resetPic(); // 重置圖片
+  starsStore.resetStars(); // 重置星星狀態
+};
 
 const openComment = () => {
-    isExpanded.value = !isExpanded.value
-}
+  isExpanded.value = !isExpanded.value;
+};
 
 const showLoginModal = ref(false); // 是否顯示登錄小畫面
 
-    // 開啟登錄小畫面
-    const openLoginModal = () => {
-      showLoginModal.value = true;
-    };
+// 開啟登錄小畫面
+const openLoginModal = () => {
+  showLoginModal.value = true;
+};
 
-    // 關閉登錄小畫面
-    const closeLoginModal = () => {
-      showLoginModal.value = false;
-    };
+// 關閉登錄小畫面
+const closeLoginModal = () => {
+  showLoginModal.value = false;
+};
 </script>
+
+<template>
+  <Login :visible="showLoginModal" @close="closeLoginModal" />
+  <div class="flex gap-5 mb-4">
+    <div class="flex-shrink-0 w-16 h-16 rounded-full bg-slate-300">
+      <img
+        src="../../assets/default_user.png"
+        alt="avatar"
+        class="w-full h-auto overflow-hidden"
+      />
+    </div>
+    <div class="flex flex-1 md:flex-none md:basis-2/3">
+      <div class="w-full text-left md:w-96">
+        <button
+          @click="openComment"
+          class="p-2 border-2 border-solid rounded-lg border-amber-500 text-amber-500"
+        >
+          留下您對餐廳的評論
+        </button>
+        <Stars class="my-2" />
+        <div class="flex flex-col relative" v-if="isExpanded">
+          <div v-if="user.userData">
+            <textarea
+              v-model="commentText"
+              maxlength="200"
+              class="w-full h-20 border-2 border-solid rounded-sm resize-none"
+              placeholder="發表用餐經驗"
+            ></textarea>
+            <p v-if="commentText.length >= 1" class="text-sm text-slate-500">
+              還可以輸入 {{ 200 - commentText.length }} 字
+            </p>
+            <div class="flex items-center w-full h-10 my-2">
+              <input
+                type="number"
+                v-model="price"
+                placeholder="輸入用餐價格"
+                class="flex-1 h-10 border-2 border-solid rounded-sm"
+              />
+              <label class="ml-2">元 / 人</label>
+            </div>
+            <UploadPic />
+            <button
+              @click="submitComment"
+              class="w-full p-2 my-2 font-bold rounded-lg shadow md:flex-1 text-amber-500"
+            >
+              送出評論
+            </button>
+          </div>
+          <div
+            v-else
+            class="flex justify-center items-center bg-white h-[100px] w-[250px] border-gray-400 border-2 rounded-xl"
+          >
+            <p class="font-black">
+              請先<button class="m-2 text-orange-400" @click="openLoginModal">
+                登入</button
+              >以留下您的評論!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
 <style scoped>
 /* 隱藏數字輸入框的小箭頭 */
 /* Chrome, Safari, Edge, Opera */
-input[type=number]::-webkit-outer-spin-button,
-input[type=number]::-webkit-inner-spin-button {
+input[type="number"]::-webkit-outer-spin-button,
+input[type="number"]::-webkit-inner-spin-button {
   -webkit-appearance: none;
   margin: 0;
 }
 /* Firefox */
-input[type=number] {
-    -webkit-appearance: none;
-    -moz-appearance: none;
-    appearance: none;
+input[type="number"] {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
 }
 </style>
--- src/components/storeComment/PreviousReview.vue
+++ src/components/storeComment/PreviousReview.vue
@@ -1,105 +1,158 @@
-<template>
-    <div class="flex flex-col gap-y-5" v-if="comments.length > 0">
-        <div class="flex flex-row gap-x-5 py-2.5 w-full max-w-screen-lg mx-auto md:px-0" v-for="(comment, index) in comments" :key="index">
-            <div class="flex-shrink-0 w-16 h-16 overflow-hidden rounded-full bg-slate-300">
-                <img :src="comment.avatar ? comment.avatar : '/src/assets/default_user.png'" alt="avatar" class="object-cover w-full h-full">
-            </div>
-            <div class="flex flex-col flex-1 w-0 text-left">
-                <router-link to="/user" class="font-bold cursor-pointer text-amber-500">Name（ reviewNum 則評論）</router-link>
-                <div class="flex gap-3">
-                    <span v-if="comment.rating" class="px-2 py-1 text-sm font-bold text-white rounded-full bg-amber-500">{{ comment.rating }}.0 ★</span>
-                    <p v-if="comment.AvgPrice">均消價位：${{ comment.AvgPrice }}</p>
-                </div>
-                <p class="text-slate-500">評論日期：{{ comment.createdAt }}</p>
-                <p class="my-3">{{ comment.content }}</p>
-                <div>
-                    <button @click="toggleLike(comment)" class="p-2 rounded-lg shadow ">likeHint</button>
-                    <button class="p-2 ml-2 rounded-lg shadow" @click="shareComment(comment)">分享評論</button>
-                </div>
-                <div class="flex gap-4 mt-4 overflow-x-auto scrollbar-hide" style="scroll-snap-type: x mandatory;">
-                    <!-- <div v-for="(imageUrl, index) in comment.pictures" :key="index" class="flex-shrink-0 w-32 h-32 overflow-hidden">
-                        <img :src="imageUrl" @click="showPopup(imageUrl)" class="object-cover w-full h-full cursor-pointer">
-                    </div> -->
-                </div>
-            </div>
-        </div>
-        <!-- Popup -->
-        <div v-if="popupImage" class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-70">
-            <div class="relative w-full max-w-screen-md">
-                <span
-                class="absolute p-2 text-black bg-white rounded-full shadow cursor-pointer top-2 right-2"
-                @click="closePopup">
-                ✕
-                </span>
-                <img :src="popupImage" class="object-contain w-1/2 m-auto max-h-1/2" />
-            </div>
-        </div>
-    </div>
-</template>
-
 <script setup>
-import axios from 'axios'
-import { ref, computed, onMounted } from 'vue'
-import { useCommentStore } from '../../stores/commentStore'
-import { useStore } from "@/stores/storePage"
+import axios from "axios";
+import { ref, computed, onMounted } from "vue";
+import { useCommentStore } from "../../stores/commentStore";
+import { useStore } from "@/stores/storePage";
 
-const store = useStore()
+const store = useStore();
 // 從 Store 獲取評論數據
-const commentStore = useCommentStore()
+const commentStore = useCommentStore();
 // const comments = computed(() => commentStore.comments)
-const comments = ref([])
+const comments = ref([]);
 
 // 圖片popup
-const popupImage = ref(null)
+const popupImage = ref(null);
 
 const showPopup = (imageUrl) => {
-    popupImage.value = imageUrl;
-}
+  popupImage.value = imageUrl;
+};
 const closePopup = () => {
-      popupImage.value = null;
-}
+  popupImage.value = null;
+};
 
-const getComment = async() => {
-    const response = await axios.get(`${import.meta.env.VITE_BACKEND_BASE_URL}/comments/restaurant/${store.placesId}`)
-    comments.value = response.data
-    
-    const userRes = await Promise.all(comments.value.map((comment) => axios.get(`${import.meta.env.VITE_BACKEND_BASE_URL}/user/${comment.userId}`)))
-    
-    
-}
+const getComment = async () => {
+  const response = await axios.get(
+    `${import.meta.env.VITE_BACKEND_BASE_URL}/comments/restaurant/${
+      store.placesId
+    }`,
+  );
+  comments.value = response.data;
 
+  const userRes = await Promise.all(
+    comments.value.map((comment) =>
+      axios.get(
+        `${import.meta.env.VITE_BACKEND_BASE_URL}/user/${comment.userId}`,
+      ),
+    ),
+  );
+  comments.value = comments.value.map((comment, index) => {
+    const user = userRes[index].data; // 假設 userRes 每個元素是完整的用戶數據
+    return {
+      ...comment, // 保留原有的 comment 屬性
+      name: user.name, // 添加用戶的名稱
+      avatar: user.profilePicture, // 添加用戶的頭像
+    };
+  });
+};
 
 // 讚數+1
 const toggleLike = (comment) => {
-    if (comment.likeStatus){
-        comment.likeNum--
-        comment.likeHint = "表示讚賞"
-    } else {
-        comment.likeNum++
-        comment.likeHint = `❤ + ${comment.likeNum}`
-    }
-    comment.likeStatus = !comment.likeStatus
-}
+  if (comment.likeStatus) {
+    comment.likeNum--;
+    comment.likeHint = "表示讚賞";
+  } else {
+    comment.likeNum++;
+    comment.likeHint = `❤ + ${comment.likeNum}`;
+  }
+  comment.likeStatus = !comment.likeStatus;
+};
 
 //分享
 const shareComment = (comment) => {
-    if(navigator.share){
-        navigator.share({
-            title: '我看到一家超讚的餐廳',
-            text: `${comment.commentText} - 評分：${comment.star} ★`,
-            url: window.location.href
-        })
-        .then(() => console.log('分享成功'))
-        .catch((error) => console.log('分享失敗：', error))
-    } else {
-        alert('您的瀏覽器不支援分享功能')
-    }
-}
+  if (navigator.share) {
+    navigator
+      .share({
+        title: "我看到一家超讚的餐廳",
+        text: `${comment.commentText} - 評分：${comment.star} ★`,
+        url: window.location.href,
+      })
+      .then(() => console.log("分享成功"))
+      .catch((error) => console.log("分享失敗：", error));
+  } else {
+    alert("您的瀏覽器不支援分享功能");
+  }
+};
 
 onMounted(() => {
-    getComment()
-}) 
-
-
-
+  getComment();
+});
 </script>
+
+<template>
+  <div class="flex flex-col gap-y-5" v-if="comments.length > 0">
+    <div
+      class="flex flex-row gap-x-5 py-2.5 w-full max-w-screen-lg mx-auto md:px-0"
+      v-for="(comment, index) in comments"
+      :key="index"
+    >
+      <div
+        class="flex-shrink-0 w-16 h-16 overflow-hidden rounded-full bg-slate-300"
+      >
+        <img
+          :src="
+            comment.avatar ? comment.avatar : '/src/assets/default_user.png'
+          "
+          alt="avatar"
+          class="object-cover w-full h-full"
+        />
+      </div>
+      <div class="flex flex-col flex-1 w-0 text-left">
+        <router-link to="/user" class="font-bold cursor-pointer text-amber-500"
+          >{{ comment.name }}（ reviewNum 則評論）</router-link
+        >
+        <div class="flex gap-3">
+          <span
+            v-if="comment.rating"
+            class="px-2 py-1 text-sm font-bold text-white rounded-full bg-amber-500"
+            >{{ comment.rating }}.0 ★</span
+          >
+          <p v-if="comment.AvgPrice">均消價位：${{ comment.AvgPrice }}</p>
+        </div>
+        <p class="text-slate-500">評論日期：{{ comment.createdAt }}</p>
+        <p class="my-3">{{ comment.content }}</p>
+        <div>
+          <button @click="toggleLike(comment)" class="p-2 rounded-lg shadow">
+            likeHint
+          </button>
+          <button
+            class="p-2 ml-2 rounded-lg shadow"
+            @click="shareComment(comment)"
+          >
+            分享評論
+          </button>
+        </div>
+        <div
+          class="flex gap-4 mt-4 overflow-x-auto scrollbar-hide"
+          style="scroll-snap-type: x mandatory"
+        >
+          <div
+            v-for="(imageUrl, index) in comment.photos"
+            :key="index"
+            class="flex-shrink-0 w-32 h-32 overflow-hidden"
+          >
+            <img
+              :src="imageUrl"
+              @click="showPopup(imageUrl)"
+              class="object-cover w-full h-full cursor-pointer"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Popup -->
+    <div
+      v-if="popupImage"
+      class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-70"
+    >
+      <div class="relative w-full max-w-screen-md">
+        <span
+          class="absolute p-2 text-black bg-white rounded-full shadow cursor-pointer top-2 right-2"
+          @click="closePopup"
+        >
+          ✕
+        </span>
+        <img :src="popupImage" class="object-contain w-1/2 m-auto max-h-1/2" />
+      </div>
+    </div>
+  </div>
+</template>
